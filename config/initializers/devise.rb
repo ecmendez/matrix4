8389ef9d9c50b5e8a242e3945e4bcdc3ed33cd37
Eva Castelo <e.castelo@asee.org>
2015-04-17 14:18:57 -0400

    browsercms integrated

---
+++ config/initializers/devise.rb
@@ -0,0 +1,7 @@
+# Configuration for Devise specific to your project.
+# BrowserCMS has configured defaults for most other values.
+Devise.setup do |config|
+
+  config.secret_key = 'd6e41b1dacfee9fb13246b6d721e45b998f6529eda40fbd99c664ab1fa2df4012352b742e695688578851213921c92ba06fcc9efa4209917356a1037e36c8d91'
+
+end
